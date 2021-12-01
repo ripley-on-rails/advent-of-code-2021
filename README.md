45afaad82c41c4d4e045b0a1f2723f9e7db4ac34
Ripley Flammer <flammer.ripley@gmail.com>
2021-12-01 17:21:37 +0100

    Add solutions for day 1 challenge

---
+++ README.md
@@ -0,0 +1,3 @@
+# advent-of-code-2021
+
+Solutions for Advent of Code 2021: https://adventofcode.com/
---
+++ src/advent_of_code_2021/day_01/core.clj
@@ -0,0 +1,26 @@
+(ns advent-of-code-2021.day-01.core
+  (:require [clojure.string :as str]))
+
+(defn slurp-input []
+  (->> (slurp "./resources/day_01/sample.txt")
+       str/split-lines
+       (map read-string)))
+
+(defn count-larger-than-previous [v]
+  (->> (map -
+            (drop 1 v)
+            (drop-last v))
+       (filter pos?)
+       count))
+
+(defn solution-part-one [samples]
+  (count-larger-than-previous samples))
+
+(defn solution-part-two [samples]
+  (count-larger-than-previous (->> samples
+                                   (partition 3 1)
+                                   (map (partial apply +)))))
+
+(let [samples (slurp-input)]
+  (println "Solution for part one:" (solution-part-one samples))
+  (println "Solution for part two:" (solution-part-two samples)))
